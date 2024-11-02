7859fd52908276cdb33995721e309c4460a1a90e
Sayantan Chowdhury <59886320+scbwn@users.noreply.github.com>
2024-11-02 17:36:40 -0400

    Update vae_mnist.py

--- vae_mnist.py
+++ vae_mnist.py
@@ -17,56 +17,6 @@ from urllib.request import urlretrieve
 %matplotlib inline 
 seed = npr.RandomState(0)
 
-# Load MNIST
-
-def download(url, filename):
-    if not os.path.exists('data'):
-        os.makedirs('data')
-    out_file = os.path.join('data', filename)
-    if not os.path.isfile(out_file):
-        urlretrieve(url, out_file)
-
-def mnist():
-    base_url = 'http://yann.lecun.com/exdb/mnist/'
-
-    def parse_labels(filename):
-        with gzip.open(filename, 'rb') as fh:
-            magic, num_data = struct.unpack(">II", fh.read(8))
-            return np.array(array.array("B", fh.read()), dtype=np.uint8)
-
-    def parse_images(filename):
-        with gzip.open(filename, 'rb') as fh:
-            magic, num_data, rows, cols = struct.unpack(">IIII", fh.read(16))
-            return np.array(array.array("B", fh.read()), 
-                            dtype=np.uint8).reshape(num_data, rows, cols)
-
-    for filename in ['train-images-idx3-ubyte.gz',
-                     'train-labels-idx1-ubyte.gz',
-                     't10k-images-idx3-ubyte.gz',
-                     't10k-labels-idx1-ubyte.gz']:
-        download(base_url + filename, filename)
-
-    train_images = parse_images('data/train-images-idx3-ubyte.gz')
-    train_labels = parse_labels('data/train-labels-idx1-ubyte.gz')
-    test_images  = parse_images('data/t10k-images-idx3-ubyte.gz')
-    test_labels  = parse_labels('data/t10k-labels-idx1-ubyte.gz')
-
-    return train_images, train_labels, test_images, test_labels
-
-
-def load_mnist():
-    partial_flatten = lambda x : np.reshape(x, (x.shape[0], np.prod(x.shape[1:])))
-    one_hot = lambda x, k: np.array(x[:,None] == np.arange(k)[None, :], dtype=int)
-    train_images, train_labels, test_images, test_labels = mnist()
-    train_images = partial_flatten(train_images) / 255.0
-    test_images  = partial_flatten(test_images)  / 255.0
-    #train_labels = one_hot(train_labels, 10)
-    #test_labels = one_hot(test_labels, 10)
-    N_data = train_images.shape[0]
-
-    return N_data, train_images, train_labels, test_images, test_labels
-
-
 def diag_gaussian_log_density(x, mu, log_std):
     return np.sum(norm.logpdf(x, mu, np.exp(log_std)), axis=-1)
 
@@ -183,7 +133,9 @@ batch_size = 200
 num_epochs = 100
 step_size = 0.001
 
+# Load MNIST
 print("Loading training data...")
+from loadMNIST import load_mnist
 N, train_images, train_labels, test_images, test_labels = load_mnist()
 def binarise(images):
     on = images > 0.5
@@ -251,29 +203,7 @@ pickle.dump(optimized_params, open("saved_params.p", "wb" ))
 
 ###############################################################################################################
 
-
-def plot_images(images, ax, ims_per_row=5, padding=5, digit_dimensions=(28, 28),
-                cmap=matplotlib.cm.binary, vmin=None, vmax=None):
-    """Images should be a (N_images x pixels) matrix."""
-    N_images = images.shape[0]
-    N_rows = np.int32(np.ceil(float(N_images) / ims_per_row))
-    pad_value = np.min(images.ravel())
-    concat_images = np.full(((digit_dimensions[0] + padding) * N_rows + padding,
-                             (digit_dimensions[1] + padding) * ims_per_row + 
-                             padding), pad_value)
-    for i in range(N_images):
-        cur_image = np.reshape(images[i, :], digit_dimensions)
-        row_ix = i // ims_per_row
-        col_ix = i % ims_per_row
-        row_start = padding + (padding + digit_dimensions[0]) * row_ix
-        col_start = padding + (padding + digit_dimensions[1]) * col_ix
-        concat_images[row_start: row_start + digit_dimensions[0],
-                      col_start: col_start + digit_dimensions[1]] = cur_image
-    cax = ax.matshow(concat_images, cmap=cmap, vmin=vmin, vmax=vmax)
-    plt.xticks(np.array([]))
-    plt.yticks(np.array([]))
-    return cax
-
+from loadMNIST import plot_images
 
 samples=np.random.randn(10,Dz)
 grscl_img = decoder(samples, opt_gen)
